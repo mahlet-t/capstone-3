25d73825b36c926e9d2e3ef40d5dbf865e7d6388
mahle <mahletwondwossen71@gmail.com>
2025-06-21 18:17:44 -0500

    Implement create method in MySqlCategoryDao to insert new category

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -53,7 +53,7 @@ public class CategoriesController {
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
-    @PreAuthorize("hasRole('ROLE-ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping()
     public Category addCategory(@RequestBody Category category) {
         // insert the category
@@ -63,7 +63,7 @@ public class CategoriesController {
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
     @PutMapping("{id}")
-    @PreAuthorize("hasRole('ROLE-ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category) {
         // update the category by id
         categoryDao.update(id, category);
@@ -73,7 +73,7 @@ public class CategoriesController {
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
     @DeleteMapping("{id}")
-    @PreAuthorize("hasRole('ROLE-ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id) {
         // delete the category by id
         categoryDao.delete(id);
--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data.mysql;
 
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
@@ -25,13 +26,12 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
         String query = "SELECT * FROM categories";
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(query);
-           ResultSet resultSet= statement.executeQuery();
-           while (resultSet.next()){
-               int categoryId= resultSet.getInt("Category_id");
-               String name=resultSet.getString("name");
-               String description=resultSet.getString("description");
-               categories.add(new Category(categoryId,name,description));
-           }
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Category category =
+                        mapRow(resultSet);
+                categories.add(category);
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -42,18 +42,41 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
     @Override
     public Category getById(int categoryId) {
         // get category by id
+        String query = "SELECT * from categories WHERE category_id =?";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, categoryId);
+            ResultSet row = statement.executeQuery();
+            if (row.next()) {
+                return mapRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public Category create(Category category) {
         // create a new category
+        String query="INSERT INTO categories (category_id,name,description) VALUES (?, ?, ?)";
+        try(Connection connection=getConnection()){
+            PreparedStatement statement= connection.prepareStatement(query);
+            statement.setInt(1, category.getCategoryId());
+            statement.setString( 2,category.getName());
+            statement.setString(3,category.getDescription());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
     @Override
     public void update(int categoryId, Category category) {
         // update category
+
     }
 
     @Override
