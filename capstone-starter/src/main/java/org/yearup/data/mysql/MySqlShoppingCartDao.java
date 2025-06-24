7d6fa94d9e950df46d7d9b7dcdc9066aa099ec5b
mahle <mahletwondwossen71@gmail.com>
2025-06-24 14:37:14 -0500

    Implement getbyuserid method in MysqlShoppingCartDao

---
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,68 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+    private ProductDao productDao;
+    public MySqlShoppingCartDao(DataSource dataSource,ProductDao productDao) {
+        super(dataSource);
+        this.productDao=productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        String query = "SELECT * FROM shopping_cart WHERE user_id=?";
+        ShoppingCart cart=new ShoppingCart();
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,userId);
+            ResultSet set = statement.executeQuery();
+            while (set.next()){
+               int productId=set.getInt("product_id");
+               int quantity=set.getInt("quantity");
+                Product product= productDao.getById(productId);
+                ShoppingCartItem item=new ShoppingCartItem();
+                item.setProduct(product);
+                item.setQuantity(quantity);
+                cart.add(item);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return cart;
+    }
+
+    @Override
+    public void addToCart(int userid, int productId) {
+
+    }
+
+    @Override
+    public ShoppingCart update(int userId, int productId, int quantity) {
+        return null;
+    }
+
+    @Override
+    public void clearCart(int userID) {
+
+    }
+
+    @Override
+    public void removeItem(int userId, int productId) {
+
+    }
+
+}
