4ce80bbfde74c5eae818424e916645fadeb2f6e0
mahle <mahletwondwossen71@gmail.com>
2025-06-24 15:06:32 -0500

    implement add to cart method in MySqlShoppingCartDao

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -16,24 +16,25 @@ import java.sql.SQLException;
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
     private ProductDao productDao;
-    public MySqlShoppingCartDao(DataSource dataSource,ProductDao productDao) {
+
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
         super(dataSource);
-        this.productDao=productDao;
+        this.productDao = productDao;
     }
 
     @Override
     public ShoppingCart getByUserId(int userId) {
         String query = "SELECT * FROM shopping_cart WHERE user_id=?";
-        ShoppingCart cart=new ShoppingCart();
+        ShoppingCart cart = new ShoppingCart();
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1,userId);
+            statement.setInt(1, userId);
             ResultSet set = statement.executeQuery();
-            while (set.next()){
-               int productId=set.getInt("product_id");
-               int quantity=set.getInt("quantity");
-                Product product= productDao.getById(productId);
-                ShoppingCartItem item=new ShoppingCartItem();
+            while (set.next()) {
+                int productId = set.getInt("product_id");
+                int quantity = set.getInt("quantity");
+                Product product = productDao.getById(productId);
+                ShoppingCartItem item = new ShoppingCartItem();
                 item.setProduct(product);
                 item.setQuantity(quantity);
                 cart.add(item);
@@ -47,11 +48,21 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public void addToCart(int userid, int productId) {
+        String query = "INSERT INTO shopping_Cart(user_id, product_id, quantity) VALUES(?, ?, 1)";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,userid);
+            statement.setInt(2,productId);
+            statement.executeUpdate();
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ShoppingCart update(int userId, int productId, int quantity) {
+
         return null;
     }
 
