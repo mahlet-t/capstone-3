846819fc2ae1f307b7263b7b0818dec238692959
mahle <mahletwondwossen71@gmail.com>
2025-06-24 13:28:57 -0500

    Completed ShoppingCartController with all endpoint and verified functionality

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -6,4 +6,8 @@ public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addToCart(int userid,int productId);
+    ShoppingCart update(int userId, int productId,int quantity);
+   void clearCart(int userID);
+    void removeItem(int userId, int productId);
 }
