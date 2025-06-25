c9434617c34ed8d2d515dafd4c8bea0fcf61f521
mahle <mahletwondwossen71@gmail.com>
2025-06-25 10:15:45 -0500

    Add order class with full fields and accessors

--- capstone-starter/src/main/java/org/yearup/models/Order.java
+++ capstone-starter/src/main/java/org/yearup/models/Order.java
@@ -1,4 +1,89 @@
 package org.yearup.models;
 
+import java.time.LocalDate;
+
 public class Order {
+    private int orderId;
+    private int userId;
+    private LocalDate date;
+    private String address;
+    private String city;
+    private String sate;
+    private String zip;
+    private String shippingAmount;
+
+    public Order(int orderId, int userId, LocalDate date, String address, String city, String sate, String zip, String shippingAmount) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.date = date;
+        this.address = address;
+        this.city = city;
+        this.sate = sate;
+        this.zip = zip;
+        this.shippingAmount = shippingAmount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSate() {
+        return sate;
+    }
+
+    public void setSate(String sate) {
+        this.sate = sate;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(String shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
 }
